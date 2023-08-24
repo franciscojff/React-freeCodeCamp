47e3424642d539c551e11732456d2ebc0e5d5c29
Francisco <franciscojff25@gmail.com>
2023-08-24 13:51:06 -0400

    Props

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import Testimonio from './components/Testimonio';
+import {Testimonio} from './components/Testimonio';
+//Exportacion nombrada, solo exporta una de las funciones del componente, por defecto exporta todo
 function App() {
   return (
     <div className="App">
-    <Testimonio/>
+    <div className='contenedor-principal'>
+    <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+    <Testimonio
+    nombre = 'Enma Bostian'
+    pais = ' Suecia'
+    imagen = ''
+    cargo = 'Ingeniera de Software'
+    empresa = 'Spotify'
+    testimonio = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,'
+    />
+    </div>
     </div>
   );
 }
--- mi-app/src/components/testimonio.js
+++ mi-app/src/components/testimonio.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import '../style-sheets/Testimonio.css'
 
-function Testimonio() {
+export function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
           src={require('../imagens/testimonio-emma.png')}
           alt='Foto de Emma'/>
       <div className='contenedor-texto-testimonio'>
-      <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-      <p className='cargo-testimonio'>Ingeniero de Software en Spotify</p>
-      <p className='texto-testimonio'> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat face  </p>
+      <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+      <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+      <p className='texto-testimonio'> {props.testimonio}  </p>
     </div>
     </div>
 
   );
 }
-export default Testimonio;
