d6681e864f4f3f7b5df92cff5e4e6b51a804cfc8
Francisco <franciscojff25@gmail.com>
2023-08-31 13:55:26 -0400

    Updated App.js

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -7,9 +7,25 @@ function App() {
     <div className='contenedor-principal'>
     <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
     <Testimonio
+    nombre = 'Shawn Wang'
+    pais = 'Singapur'
+    imagen = 'shawn'
+    cargo = 'Ingeniero de Software'
+    empresa = 'Uber'
+    testimonio = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,'    
+    />
+    <Testimonio
+    nombre = 'Sarah Waallys'
+    pais = 'Nigeria'
+    imagen = 'sarah'
+    cargo = 'Ingeniera de Software'
+    empresa = 'Chatdesk'
+    testimonio = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,'    
+    />        
+    <Testimonio
     nombre = 'Enma Bostian'
     pais = ' Suecia'
-    imagen = ''
+    imagen = 'emma'
     cargo = 'Ingeniera de Software'
     empresa = 'Spotify'
     testimonio = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,'
--- mi-app/src/components/testimonio.js
+++ mi-app/src/components/testimonio.js
@@ -5,11 +5,11 @@ export function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
-          src={require('../imagens/testimonio-emma.png')}
+          src={require(`../imagens/testimonio-${props.imagen}.png`)}
           alt='Foto de Emma'/>
       <div className='contenedor-texto-testimonio'>
-      <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-      <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+      <p className='nombre-testimonio'> <strong> {props.nombre}</strong> en {props.pais}</p>
+      <p className='cargo-testimonio'>{props.cargo} en <strong> {props.empresa}</strong></p>
       <p className='texto-testimonio'> {props.testimonio}  </p>
     </div>
     </div>
