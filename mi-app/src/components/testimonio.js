56ad43d6de1a5cd6205cfb37e64efd99941ffde3
Francisco <franciscojff25@gmail.com>
2023-09-01 18:38:47 -0400

    Agregar proyecto - Contador de Clics

--- mi-app/src/components/testimonio.js
+++ mi-app/src/components/testimonio.js
@@ -6,7 +6,7 @@ export function Testimonio(props) {
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
           src={require(`../imagens/testimonio-${props.imagen}.png`)}
-          alt='Foto de Emma'/>
+          alt={`Foto de ${props.imagen}`}/>
       <div className='contenedor-texto-testimonio'>
       <p className='nombre-testimonio'> <strong> {props.nombre}</strong> en {props.pais}</p>
       <p className='cargo-testimonio'>{props.cargo} en <strong> {props.empresa}</strong></p>
